a0ed37eeff6a89d28e8d97a14e47db8bf7c7059b
anxrudhmishra <anxrudhmishra@gmail.com>
2026-04-17 22:11:51 +0530

    my First software feature test

--- software_test.py
+++ software_test.py
@@ -1,2 +1,10 @@
 # Test 1
 # Letting the user to know what type of data they entered as a input
+# Failure 1 entered wrong instructions
+# Failure 2 wrong logic setup
+# Failure 3 Forgot to include a closing bracket in print() function
+# Failure 4 setup problem occurs simultaneously
+# Success 1 Finally my code works
+user = input("Enter your name:")
+data_type = input("Enter 'name': ")
+print("Data you have entered is,",type('name'))
