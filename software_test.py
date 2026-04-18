4d387266ec8136e5d0fd5327672e4fa2e35f5a72
anxrudhmishra <anxrudhmishra@gmail.com>
2026-04-18 11:05:04 +0530

    Organised my logs

--- software_test.py
+++ software_test.py
@@ -1,13 +1,28 @@
+  # ------------------------------------------- #
+                     # Tests
+ #  ------------------------------------------- #
 # Test 1
-# Feature 1 Letting the user to know what type of data they entered as a input
-# Feature 2 Error handling.
+
+# (Feature 1 Letting the user to know what type of data they entered as a input)
+
+# Test 2
+
+# (Feature 2 Error handling.)
+
+# --------------------------------------------- #
+                    # Failures
+# --------------------------------------------- #
+
 # Failure 1 entered wrong instructions
 # Failure 2 wrong logic setup
 # Failure 3 Forgot to include a closing bracket in print() function
 # Failure 4 setup problem occurs simultaneously
 # Failure 5 wrong instructions
+
+# -------------------------------------------- #
+                   # Success
+# -------------------------------------------- #
+
 # Success 1 Finally my code works
 # success 2 It worked
-user = input("Enter your name:")
-data_type = input("Enter 'user': ")
-print("Data you have entered is,",type(user))
+
